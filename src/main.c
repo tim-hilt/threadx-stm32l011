f44558a00a2abb8dafed68851d30e71ce66de71f
Tim Hilt <timhilt@live.de>
2025-01-26 22:27:46 +0100

    read/write to flash

--- src/main.c
+++ src/main.c
@@ -13,16 +13,64 @@ void init_leds(void);
 void toggle_on(uint32_t);
 void toggle_off(uint32_t);
 void increase_clock_speed(void);
+void UnlockPELOCK(void);
+void UnlockPRGLOCK(void);
+
+#define FLASH_PEKEY1 (0x89ABCDEFU)
+#define FLASH_PEKEY2 (0x02030405U)
+#define FLASH_PRGKEY1 (0x8C9DAEBFU)
+#define FLASH_PRGKEY2 (0x13141516U)
+
+extern uint32_t _edata;
 
 int main(void) {
   init_leds();
   init_uart();
   increase_clock_speed();
+  UnlockPELOCK();
+  UnlockPRGLOCK();
+
   tx_kernel_enter();
 }
 
+__INLINE void UnlockPELOCK(void) {
+  while ((FLASH->SR & FLASH_SR_BSY) != 0);
+  if ((FLASH->PECR & FLASH_PECR_PELOCK) != 0) {
+    FLASH->PEKEYR = FLASH_PEKEY1;
+    FLASH->PEKEYR = FLASH_PEKEY2;
+  }
+}
+
+__INLINE void UnlockPRGLOCK(void) {
+  while ((FLASH->SR & FLASH_SR_BSY) != 0);
+  if ((FLASH->PECR & FLASH_PECR_PELOCK) == 0) {
+    if ((FLASH->PECR & FLASH_PECR_PRGLOCK) != 0) {
+      FLASH->PRGKEYR = FLASH_PRGKEY1;
+      FLASH->PRGKEYR = FLASH_PRGKEY2;
+    }
+  }
+}
+
+void FlashWord32Prog(uint32_t flash_addr, uint32_t data) {
+  *(__IO uint32_t*)(flash_addr) = data;
+
+  while ((FLASH->SR & FLASH_SR_BSY) != 0);
+
+  if ((FLASH->SR & FLASH_SR_EOP) != 0) {
+    FLASH->SR = FLASH_SR_EOP;
+  } else if ((FLASH->SR & FLASH_SR_FWWERR) != 0) {
+    FLASH->SR = FLASH_SR_FWWERR;
+  } else if ((FLASH->SR & FLASH_SR_NOTZEROERR) != 0) {
+    FLASH->SR = FLASH_SR_NOTZEROERR;
+  } else if ((FLASH->SR & FLASH_SR_SIZERR) != 0) {
+    FLASH->SR = FLASH_SR_SIZERR;
+  } else if ((FLASH->SR & FLASH_SR_WRPERR) != 0) {
+    FLASH->SR = FLASH_SR_WRPERR;
+  }
+}
+
 // TODO: Try to understand every step that's done here
-void increase_clock_speed(void) {
+__INLINE void increase_clock_speed(void) {
   FLASH->ACR |= FLASH_ACR_LATENCY;
   while ((FLASH->ACR & FLASH_ACR_LATENCY) == 0);
   // TODO: Enable UART and print out the amount of time it took for the while
@@ -38,12 +86,12 @@ void increase_clock_speed(void) {
   SysTick_Config(32000);
 }
 
-void init_leds(void) {
+__INLINE void init_leds(void) {
   RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
   GPIOB->MODER ^= GPIO_MODER_MODE3_1;
 }
 
-void init_uart(void) {
+__INLINE void init_uart(void) {
   RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
 
   GPIOA->MODER &= ~GPIO_MODER_MODE2_Msk;
@@ -116,15 +164,31 @@ void toggle_on(uint32_t thread_input) {
   while (1) {
     GPIOB->BSRR = GPIO_BSRR_BS_3;
     print("on\n");
+    FlashWord32Prog((uint32_t)&_edata, *((uint32_t*)&_edata) + 1);
     tx_thread_sleep(1000);
   }
 }
 
 void toggle_off(uint32_t thread_input) {
+  uint32_t data;
+  char c[1];
+  uint32_t divisor = 1000000000U;
+
   while (1) {
     tx_thread_sleep(500);
     GPIOB->BSRR = GPIO_BSRR_BR_3;
     print("off\n");
+
+    data = *((uint32_t*)&_edata);
+
+    while (divisor > 0) {
+      c[0] = '0' + ((data / divisor) % 10);
+      print(c);
+      divisor /= 10;
+    }
+    print("\n");
+    divisor = 1000000000U;
+
     tx_thread_sleep(500);
   }
 }
